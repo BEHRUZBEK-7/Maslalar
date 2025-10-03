9cd0a6feb54cdd38a1f2054a864d7e4e19d10e00
BEHRUZBEK JUMABOYEV <jumaboyevbehruz148@gmail.com>
2025-10-03 15:36:47 +0500

    Update Masala.Yechimi(03.10.2025).py

--- Masala.Yechimi(03.10.2025).py
+++ Masala.Yechimi(03.10.2025).py
@@ -5,8 +5,8 @@
 # print(f"yigindi : {a+b}")
 
 #2
-# familiya = input("familyangizni kiriting: ")
-# ism = input("ismingizni kiriting: ")
-# sharif = input("sharifingizni kiriting: ")
-#
-# print(f"F.I.SH.{familiya} {ism} {sharif}")
+familiya = input("familyangizni kiriting: ")
+ism = input("ismingizni kiriting: ")
+sharif = input("sharifingizni kiriting: ")
+
+print(f"F.I.SH:{familiya} {ism} {sharif}".title())
