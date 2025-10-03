2e64ee75ad7d4f38035d99226bcde41a8b52a081
BEHRUZBEK JUMABOYEV <jumaboyevbehruz148@gmail.com>
2025-10-03 15:23:19 +0500

    masala yechimi

--- Masala.Yechimi(03.10.2025).py
+++ Masala.Yechimi(03.10.2025).py
@@ -3,3 +3,12 @@
 # b = int(input("ikkinchi sonni kiriting : "))
 #
 # print(f"yigindi : {a+b}")
+
+#2
+# familiya = input("familyangizni kiriting : ")
+# ism = input("ismingizni kiriting : ")
+# sharif = input("sharifingizni kiriting : ")
+#
+# print(familiya)
+# print(ism)
+# print(sharif)
